85a6c33c634401e6f04d458b34840c2aa4f42b81
Julietamorales <127864219+Julietamorales@users.noreply.github.com>
2026-02-01 20:37:02 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+const noBtn = document.getElementById("noBtn");
+const yesBtn = document.getElementById("yesBtn");
+const message = document.getElementById("message");
+
+noBtn.addEventListener("mouseover", () => {
+  const x = Math.random() * (window.innerWidth - noBtn.offsetWidth);
+  const y = Math.random() * (window.innerHeight - noBtn.offsetHeight);
+
+  noBtn.style.left = `${x}px`;
+  noBtn.style.top = `${y}px`;
+});
+
+yesBtn.addEventListener("click", () => {
+  message.innerHTML = "Sabía que dirías que sí 💕🥰";
+});
